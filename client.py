ef2f88f4422897e4b220294c676b600ca0796026
Henry Wang <wanghenry1998@gmail.com>
2020-04-08 02:08:20 -0500

    Add v1 to api path

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import requests
 import json
 import hashlib
 
-base_url = 'http://localhost:3000/api'
+base_url = 'http://localhost:3000/api/v1'
 
 emp_creds = {
     'email': 'adowley0@myspace.com',
