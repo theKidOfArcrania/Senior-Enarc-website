6a613b42612f5df8d1cf1dacc54d9e4f718ce3f0
Henry Wang <wanghenry1998@gmail.com>
2020-04-12 14:36:43 -0500

    Improve testing/coverage, for /teams
     * Add more specific fail keywords in endpoints
     * Suppress all console log output on testing
     * Force /team to return the team that the user's team if that user is a student
     * Return success (instead of fail) for no teams to view
     * Fix /team/list endpoint, returning a message object now

--- test/server.js
+++ test/server.js
@@ -1,6 +1,5 @@
 const request = require('supertest');
 const assert = require('assert');
-const {getInst} = require('../lib/model/db.js');
 
 const config = require('../lib/config.js');
 config.TESTING = true;
@@ -14,6 +13,7 @@ const eDowley = 'adowley0@myspace.com';
 const eBrown = 'tbrownjohn7@cdbaby.com';
 const eStennes = 'hstennesa@cmu.edu';
 const eVivianne = 'vweine4@ox.ac.uk';
+const eDarline = 'deric8@un.org';
 
 const eBrownPass =
     'e2fb7d22771b5e55d4707630c62420eea3a2904847f290eea627a7b9e7ded495';
@@ -31,6 +31,10 @@ uspecs = {
     admin: false, utd: true, employee: false,
     utype: 'staff', name: 'Stennes, Halli',
   },
+  [eDarline]: {
+    admin: false, utd: true, employee: true,
+    utype: 'student', name: 'Eric, Darline',
+  },
   [eVivianne]: {
     admin: false, utd: false, employee: true,
     utype: null, name: 'Weine, Vivianne',
@@ -43,13 +47,6 @@ describe('server', function() {
 
   before(async function() {
     server = await require('../lib/server.js')();
-
-    const db = getInst();
-    // Vivianne mentors project 7, team 4
-    await db.alterTeamInfo(4, {assignedProj: 7});
-
-    u = new (require('../lib/model/user.js').User)(6);
-    await u.reload();
   });
 
   after(function() {
@@ -103,6 +100,7 @@ describe('server', function() {
     it('by default you\'re not authenticated.', async function() {
       const resp = await json.get('/api/v1/checksess');
       assert(!resp.success);
+      assert.strictEqual(resp.body.debug, 'nologin');
     });
 
     it('authenticated after login, includes information', async function() {
@@ -128,6 +126,7 @@ describe('server', function() {
         password: eBrownPass});
       assert.strictEqual(agent.jar.getCookies().length, 0);
       assert(!r1.success);
+      assert.strictEqual(r1.body.debug, 'nonemployee');
     });
 
     it('logout should no longer have session', async function() {
@@ -138,6 +137,7 @@ describe('server', function() {
       assert(r2.success);
       const r3 = await json.get('/api/v1/checksess');
       assert(!r3.success);
+      assert.strictEqual(r3.body.debug, 'nologin');
     });
 
     it('should not auth with invalid email', async function() {
@@ -146,6 +146,7 @@ describe('server', function() {
         password: eBrownPass});
       assert.strictEqual(agent.jar.getCookies().length, 0);
       assert(!r1.success);
+      assert.strictEqual(r1.body.debug, 'nouser');
     });
 
     it('should not auth with invalid password', async function() {
@@ -154,6 +155,7 @@ describe('server', function() {
         password: 'badpass'});
       assert.strictEqual(agent.jar.getCookies().length, 0);
       assert(!r1.success);
+      assert.strictEqual(r1.body.debug, 'badpassword');
     });
   });
 
@@ -162,11 +164,13 @@ describe('server', function() {
       it('should deny access without login', async function() {
         const r1 = await json.get('/api/v1/team');
         assert(!r1.success);
+        assert.strictEqual(r1.body.debug, 'nologin');
       });
       it('tbrown has no teams', async function() {
         await doLogin(eBrown);
         const r1 = await json.get('/api/v1/team');
-        assert(!r1.success);
+        assert(r1.success);
+        assert.strictEqual(r1.body, null);
       });
       it('dowley is in team 39', async function() {
         await doLogin(eDowley);
@@ -185,15 +189,29 @@ describe('server', function() {
         assert(found);
         assert.strictEqual(found.email, eDowley);
       });
-      it('dowley can access team 39 only', async function() {
-        await doLogin(eDowley);
-        const list = [];
-        for (let i = 0; i <= 52; i++) list.push(i);
-
-        const r1 = await json.post('/api/v1/team', list);
+      it('Vivianne (non UTD employees) will see first team that they are in',
+          async function() {
+            await doLogin(eVivianne);
+            const r1 = await json.get('/api/v1/team');
+            assert(r1.success);
+            assert.strictEqual(r1.body.tid, 7);
+          });
+      it('Vivianne is not in the list of team.members', async function() {
+        await doLogin(eVivianne);
+        const r1 = await json.get('/api/v1/team');
         assert(r1.success);
-        const teams = Object.keys(r1.body);
-        assert.deepStrictEqual(teams, ['39']);
+        for (const m of r1.body.members) {
+          assert.notStrictEqual(m.userId, 6);
+        }
+      });
+      it('dowley can access own teams only', async function() {
+        await doLogin(eDowley);
+        const r1 = await json.post('/api/v1/team', util.range(0, 53));
+        assert(r1.success);
+        const teams = Object.keys(r1.body)
+            .map((n) => parseInt(n))
+            .nsort();
+        assert.deepStrictEqual(teams, [6, 39, 42, 50]);
       });
       it('tbrown (admins) can access all teams', async function() {
         await doLogin(eBrown);
@@ -204,7 +222,7 @@ describe('server', function() {
         assert(r1.success);
         const teams = Object.keys(r1.body)
             .map((n) => parseInt(n))
-            .sort((a, b) => a - b);
+            .nsort();
         assert.deepStrictEqual(teams, util.range(1, 51));
       });
       it('stennes (staff) can access all teams', async function() {
@@ -213,15 +231,45 @@ describe('server', function() {
         assert(r1.success);
         const teams = Object.keys(r1.body)
             .map((n) => parseInt(n))
-            .sort((a, b) => a - b);
+            .nsort();
         assert.deepStrictEqual(teams, util.range(1, 51));
       });
-      it('Vivianne has no access to teams', async function() {
+      it('Vivianne has access to her own teams', async function() {
         await doLogin(eVivianne);
         const r1 = await json.post('/api/v1/team', util.range(0, 53));
         assert(r1.success);
-        const teams = Object.keys(r1.body);
-        assert.deepStrictEqual(teams, ['4']);
+        const teams = Object.keys(r1.body)
+            .map((n) => parseInt(n))
+            .nsort();
+        assert.deepStrictEqual(teams, [7, 39, 49]);
+      });
+    });
+    describe('/team/list', function() {
+      it('should deny access without login', async function() {
+        const r1 = await json.get('/api/v1/team/list');
+        assert(!r1.success);
+        assert.strictEqual(r1.body.debug, 'nologin');
+      });
+      it('stennes (staff) can list all teams', async function() {
+        await doLogin(eStennes);
+        const r1 = await json.get('/api/v1/team/list');
+        assert(r1.success);
+        assert.deepStrictEqual(r1.body.nsort(),
+            util.range(1, 51));
+      });
+      it('darline gets access to public teams', async function() {
+        await doLogin(eDarline);
+        const r1 = await json.get('/api/v1/team/list');
+        assert(r1.success);
+        assert.deepStrictEqual(r1.body.nsort(), util.range(3, 51));
+      });
+    });
+    describe('/team/mylist', function() {
+      it('should return teams with member of/mentoring/etc', async function() {
+        await doLogin(eDowley);
+        const r1 = await json.get('/api/v1/team/mylist');
+        assert(r1.success);
+        assert.deepStrictEqual(r1.body.nsort(), [6, 39, 42, 50]);
       });
     });
   });
